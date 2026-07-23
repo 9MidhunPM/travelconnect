0b31583be196a6f5ff37a2e5a5ccdd9c0f9a748c
9MidhunPM <midhun.titan@gmail.com>
2026-07-23 21:50:07 +0530

    Fix About page text color to black

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "20px" }}>
+    <div style={{ backgroundColor: "white", color: "black", minHeight: "100vh", padding: "20px" }}>
       <h1>About Page</h1>
       <p>This is my first nextjs work. Im Midhun PM from CSD</p>
     </div>
